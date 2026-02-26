d46c0b598440d5f518b91a7b62c706fb62b13a78
Lalit_mohan <lalitg6376@gmail.com>
2026-02-26 10:07:51 +0530

    Update unit4.py

--- unit4.py
+++ unit4.py
@@ -15,7 +15,7 @@ df.rename(columns={'Exp^rience':'Exprience'},inplace=True)
 # print(df.dtypes)
 
 #convert column to integer or float
- 
+
 
 df['Age'] = df['Age'].fillna(df['Age'].median())
 df['Salary'] = df['Salary'].fillna(df['Salary'].median())
@@ -23,8 +23,8 @@ df.fillna(method='bfill',inplace=True)
 df = df[df['Age']<100]
 
 df.drop_duplicates(inplace=True)
-
 df['Name'] = df['Name'].fillna('unknown')
 scale = MinMaxScaler()
 df[['Age','Salary']] = scale.fit_transform(df[['Age','Salary']])
+
 print(df)
